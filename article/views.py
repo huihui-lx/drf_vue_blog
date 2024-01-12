3e57e7c4d60e8118c6b6b93cbbc3bc1626dc3d82
liuxin <1652020353@qq.com>
2024-01-12 14:57:35 +0800

    基于类的视图

--- article/views.py
+++ article/views.py
@@ -1,21 +1,15 @@
-from django.http import JsonResponse
+from rest_framework import generics
+
 from article.models import Article
+from article.serializers import ArticleDetailSerializer
 from article.serializers import ArticleListSerializer
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
 
-@api_view(['GET', 'POST'])
-def article_list(request):
-    if request.method == 'GET':
-        articles = Article.objects.all()
-        serializer = ArticleListSerializer(articles, many=True)
-        return Response(serializer.data)
+class ArticleList(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleListSerializer
 
-    elif request.method == 'POST':
-        serializer = ArticleListSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleDetailSerializer
