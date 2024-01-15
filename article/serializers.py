0f7ac56d69025b22c486ca8b79d6cc9de9a272a4
liuxin <1652020353@qq.com>
2024-01-15 09:16:35 +0800

    超链接与分页

--- article/serializers.py
+++ article/serializers.py
@@ -7,11 +7,15 @@ from user_info.serializers import UserDescSerializer
 class ArticleListSerializer(serializers.ModelSerializer):
     # read_only 参数设置为只读
     author = UserDescSerializer(read_only=True)
+    # 新增字段，添加超链接
+    url = serializers.HyperlinkedIdentityField(view_name="article:detail")
 
     class Meta:
         model = Article
         fields = [
-            'id',
+            # 有了 url 之后，id 就不需要了
+            # 'id',
+            'url',
             'title',
             'created',
             'author',
